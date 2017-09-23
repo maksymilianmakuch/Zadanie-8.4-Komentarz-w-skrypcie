7f75d5324d7c1df5845421755dcb6064bb862115
Maksym <maksymilian.makuch7@gmail.com>
2017-09-23 18:43:33 +0200

    pierwsza zmiana

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+var name = prompt('Podaj swoje imię');
+//komentarz liniowy alert('Cześć, ' + name);
+console.log('Cześć, ' + name);
+
+/*
+	komentarz blokowy - zadaniowy komentarz nr 1
+*/
+
+/*
+	komentarz blokowy - zadaniowy komentarz nr 2
+	console.log('Cześć, ' + name);
+*/
